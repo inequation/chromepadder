a2fcb14c800d918e6c2542a01050709c982b366a
Leszek Godlewski <lg@inequation.org>
2012-10-06 18:50:25 +0200

    stub code for the Kinect NPAPI plugin

---
+++ kinectplugin/cpkinect.cpp
@@ -0,0 +1,9 @@
+#include "cpkinect.h"
+
+float ChromePadderKinect::GetScrollX() {
+  return 0.f;
+}
+
+float ChromePadderKinect::GetScrollY() {
+  return 0.f;
+}
---
+++ kinectplugin/cpkinect.h
@@ -0,0 +1,11 @@
+#ifndef CPKINECT_H
+#define CPKINECT_H
+
+class ChromePadderKinect {
+    public:
+        ChromePadderKinect() {}
+        float GetScrollX();
+        float GetScrollY();
+};
+
+#endif // CPKINECT_H
